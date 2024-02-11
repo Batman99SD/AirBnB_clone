37d176f87aeb5214100b2e29aeb192050431e0e8
Batman99SD <azozkrg1@gmail.com>
2024-02-11 13:59:31 -0800

    add basemodel

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""BaseModel class"""
+
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        data = self.__dict__.copy()
+        data["created_at"] = self.created_at.isoformat()
+        data["updated_at"] = self.updated_at.isoformat()
+        data["__class__"] = self.__class__.__name__
+        return (data)
