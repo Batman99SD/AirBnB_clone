72482b143603b859a148ec3b67927b78919ee069
Batman99SD <azozkrg1@gmail.com>
2024-02-11 16:54:59 -0800

    add review

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.review import Review
+from models.amenity import Amenity
 
 class FileStorage:
     __file_path = "file.json"
